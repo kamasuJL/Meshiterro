277694447ac98e4678eed21d3d76d84c0cf2e7ff
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-02-08 11:42:44 +0000

    [Finish]1~7

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get '/homes/about' => 'homes#about', as: 'about'
+
+  # 以下3行の記載なしでログアウトできなかったので追記してます（AI質問
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
