bfd78afefbdc0354be25825880d481c53c9156d1
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-02-09 09:22:07 +0000

    [Finish]15,16

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
   devise_for :users
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # 以下3行の記載なしでログアウトできなかったので追記してます（AI質問
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
