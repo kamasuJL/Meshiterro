1620d31c48015245aec48444201d23d7d18e7622
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-02-10 07:30:00 +0000

    [Finish]17,18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(user.id)
+    redirect_to user_path(@user.id)
   end
 
 
@@ -21,3 +20,4 @@ end
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   get "/homes/about" => "homes#about", as: "about"
 
   resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  # 以下3行の記載なしでログアウトできなかったので追記してます（AI質問
-  devise_scope :user do
-    get '/users/sign_out', to: 'devise/sessions#destroy'
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
